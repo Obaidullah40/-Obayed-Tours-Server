b8a7addf961fbafe7ab177b5cd7f3710c8a78daa
Sheikh Md Obaidullah <obaidullah2p17@gmail.com>
2021-10-31 23:39:25 +0600

    2 commit

--- src/pages/Booking/MyOrders/ShowOrder/ShowOrder.js
+++ src/pages/Booking/MyOrders/ShowOrder/ShowOrder.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Col } from 'react-bootstrap';
-import DeleteOrder from '../DeleteOrder/DeleteOrder';
+
 
 
 const ShowOrder = ({ showOrder }) => {
+    const [offers, setOffers] = useState([]);
     const { name, phone, email, address } = showOrder;
-    
+
+    // DELETE AN USER
+    const handleDelete = (id) => {
+        const proceed = window.confirm("Are you sure, you want to delete?");
+        if (proceed) {
+            const url = `http://localhost:5000/books/${id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.deletedCount > 0) {
+                        window.confirm("Are You sure, You want to delete");
+                        const remaining = offers.filter(
+                            (offer) => offer._id !== id
+                        );
+                        setOffers(remaining);
+                        alert("Deleted Successfully");
+                        
+                    }
+                    console.log(data);
+                });
+        }
+    };
     return (
         <div className="service">
             <Col>
@@ -19,10 +43,16 @@ const ShowOrder = ({ showOrder }) => {
                             <p>{phone}</p>
                             <p>{address}</p>
                         </Card.Text>
-                        <Card.Link href="#"></Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
+
+                        <Card.Link href="#">
+                            <button
+                                className="bg-warning"
+                                onClick={() => handleDelete(showOrder._id)}
+                            >
+                                Delete
+                            </button>
+                        </Card.Link>
                     </Card.Body>
-                    
                 </Card>
             </Col>
         </div>
